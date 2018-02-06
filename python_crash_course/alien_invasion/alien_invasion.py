029967faa647f528a3a7b59a7fac526d49b65ccf
Stephen Donovan <steviedons@gmail.com>
2018-02-06 16:29:58 +0000

    started the alien invasion game from the crash course book

---
+++ python_crash_course/alien_invasion/alien_invasion.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+from alien_settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init()
+
+    ai_settings = Settings()    
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+
+    # Start the main loop for the game
+    while True:
+
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+run_game()
