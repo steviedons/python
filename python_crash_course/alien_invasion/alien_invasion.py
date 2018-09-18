71efd132a92300bca94eafc06c97b7f8dbb749b4
Stephen Donovan <steviedons@gmail.com>
2018-09-18 17:25:30 +0100

    Started adding the bullet class to the alien game

--- python_crash_course/alien_invasion/alien_invasion.py
+++ python_crash_course/alien_invasion/alien_invasion.py
@@ -8,7 +8,7 @@ def run_game():
     # Initialize game and create a screen object
     pygame.init()
 
-    ai_settings = Settings()    
+    ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
--- python_crash_course/alien_invasion/game_functions.py
+++ python_crash_course/alien_invasion/game_functions.py
@@ -35,3 +35,4 @@ def update_screen(ai_settings, screen, ship):
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
+    
