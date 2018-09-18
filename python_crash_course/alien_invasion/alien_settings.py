c0bb68aab9c0f64e5d827aeb4293a065174e977c
Stephen Donovan <steviedons@gmail.com>
2018-09-18 21:22:57 +0100

    laptop sync

--- python_crash_course/alien_invasion/alien_settings.py
+++ python_crash_course/alien_invasion/alien_settings.py
@@ -6,6 +6,6 @@ class Settings():
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 0)   
+        self.bg_color = (0, 0, 0)
         # Ship settings
         self.ship_speed_factor = 1.5
--- python_crash_course/alien_invasion/game_functions.py
+++ python_crash_course/alien_invasion/game_functions.py
@@ -3,7 +3,7 @@ import pygame
 
 def check_events(ship):
     """Respond to keypresses and mouse events"""
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
