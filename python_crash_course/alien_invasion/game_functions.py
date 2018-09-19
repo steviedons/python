acb4c45fc4146fd22a5b4cc7cb6101d77d9c6f87
Stephen Donovan <steviedons@gmail.com>
2018-09-19 16:16:40 +0100

    Ch12 finished

--- python_crash_course/alien_invasion/game_functions.py
+++ python_crash_course/alien_invasion/game_functions.py
@@ -20,10 +20,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
 
@@ -52,4 +49,10 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if bullet limit has not been reached"""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
