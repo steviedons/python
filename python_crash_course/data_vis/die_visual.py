c3ae776b8ff92ef52869557c097839992c2b115a
Stephen Donovan <steviedons@gmail.com>
2018-09-20 17:11:22 +0100

    Ch 15 matplotlib and pygal finished

---
+++ python_crash_course/data_vis/die_visual.py
@@ -0,0 +1,33 @@
+from die import Die
+import pygal
+
+# Creat a 6 sided die
+die_1 = Die(12)
+die_2 = Die(10)
+
+# Make some rolls and store the results in a list
+#results = []
+
+#for roll_number in range(100):
+#    result = die.roll()
+#    results.append(result)
+
+results = [die_1.roll() + die_2.roll() for n in range(1000000)]
+max_results = die_1.num_sides + die_2.num_sides
+frequencies = [results.count(n) for n in range(2, max_results+1)]
+
+#print(results)
+print(frequencies)
+
+# Visualize the results
+
+hist = pygal.Bar()
+
+
+hist.title = "Results of rolling two different size die 10000 times"
+hist.x_labels = [str(x) for x in range(2, die_1.num_sides + die_2.num_sides + 1)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D' + str(die_1.num_sides) + ' + ' + 'D' + str(die_2.num_sides), frequencies)
+hist.render_to_file('die_visual.svg')
--- python_crash_course/data_vis/simple_plot.py
+++ python_crash_course/data_vis/simple_plot.py
@@ -1,5 +1,19 @@
-import matplotlib
+import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+# input_values = [1, 2, 3, 4, 5]
+# squares = [1, 4, 9, 16, 25]
+
+input_values = list(range(1,1001))
+squares = [x**2 for x in input_values]
+
+plt.plot(input_values, squares, linewidth=5)
+
+# Set chart title and lebel axis
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
